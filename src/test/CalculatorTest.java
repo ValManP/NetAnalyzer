5b052ab812c96170d38fa850af6d31da55b76698
ValManP <valpozd@mail.ru>
2017-01-28 00:12:15 +0300

    First step

---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        // Arrange
+        Calculator calc = new Calculator();
+        int a = 2;
+        int b = 4;
+
+        // Act
+        int result = calc.add(a, b);
+        int expectedResult = 6;
+
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+}
