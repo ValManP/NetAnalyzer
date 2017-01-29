efab537852ba570fac14c52ee40ce54435642bed
ValManP <valpozd@mail.ru>
2017-01-29 23:18:02 +0300

    checkstyle

--- src/main/java/tools/Calculator.java
+++ src/main/java/tools/Calculator.java
@@ -4,16 +4,14 @@ package tools;
 /**
  * Calculator
  */
-public class Calculator
-{
+public class Calculator {
     /**
      *
      * @param a first elem
      * @param b second elem
      * @return sum
      */
-    public int add ( int a, int b )
-    {
+    final public int add (final int a, final int b) {
         return a + b;
     }
 }
